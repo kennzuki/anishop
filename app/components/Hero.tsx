a587140657410177572ed72620ba6113bb797a1e
kennzuki <kingsyken@gmail.com>
2025-02-26 16:52:00 +0300

    adjusting padding and height od components

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-      <div className='bg-green-500 text-white p-12 flex justify-between gap-7 h-64'> 
+      <div className='bg-green-500 text-white grid grid-cols-1 sm:grid-cols-2 items-center p-12 justify-between gap-6 sm:h-24 md:h-64 text-center'> 
           <section className="">
               <h2 className="text-3xl font-bold">Anisoft</h2>
               <p className="">Get your pets daycare sorted</p>
           </section>
-          <section className="">
-              <p className="font-semibold capitalize">number of guests <span className="rounded-full p-4 font-bold text-2xl bg-gray-200 h-[45px] w-[45px] text-black">2</span></p>
+          <section className="flex flex-col gap-4">
+              <p className="font-semibold capitalize ">number of guests <span className="rounded-full p-4 font-bold text-2xl bg-gray-200 h-[45px] w-[45px] text-black text-center">2</span></p>
           </section>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='grid grid-cols-1 sm:grid-cols-2 min-h-screen justify-between p-8 gap-6 place-items-center bg-white'>
-      <section className="w-full">
+    <main className='grid grid-cols-1 sm:grid-cols-2  justify-between p-8 gap-6 place-items-center bg-white'>
+      <section className="w-full p-4">
         <Image  className="rounded-xl " src='https://images.unsplash.com/photo-1620021030259-977a6aa9006f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBldCUyMGRheWNhcmV8ZW58MHx8MHx8fDA%3D' alt='logo' width={450} height={450} />
       </section>
-      <section className="flex flex-col gap-4 p-8 ">
+      <section className="flex flex-col gap-4 p-4 ">
         <h2 className="text-3xl font-bold mb-4 text-black text-center w-1/2 mx-auto">Take the stress out of your leaving pets alone with anisoft daycare</h2>
         <article className="[&_button]:py-2 [&_button]:px-4 [&_button]:text-white [&_button]:rounded-md text-center flex place-items-center gap-4 justify-center">
           <button className="bg-black text-white">Get started</button>
