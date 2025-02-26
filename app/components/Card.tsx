e6d85365102a1951e180ef11c28b482e03c9dd05
kennzuki <kingsyken@gmail.com>
2025-02-26 19:44:09 +0300

    display pet with fetch

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,21 +1,30 @@
 import Image from 'next/image';
 import React from 'react';
+import { Pet } from '../lib/types';
 
-export const Card = () => {
+
+
+export default async function Card() {
+    const data = await fetch('https://bytegrad.com/course-assets/projects/petsoft/api/pets')
+    const pets = await data.json()
+    console.log(pets)
   return (
-    <ul className='bg-white border border-gray-200 rounded-xl shadow-md'>
-      <li className=''>
-        <button className='flex gap-6 place-items-center p-8'>
-          <Image
-            className='w-[65px] h-[65px] rounded-full bg-gray-400 p-4'
-            src='https://plus.unsplash.com/premium_photo-1668606717900-0ecf91e55655?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZG9nc3xlbnwwfHwwfHx8MA%3D%3Dg'
-            width={45}
-            height={45}
-            alt='image'
-          />
-          <h2 className='text-lg font-bold'>Benjamin</h2>
-        </button>
-      </li>
+      <ul className='bg-white border border-gray-200 rounded-xl shadow-md'>
+          {pets.map((pet:Pet) => (
+              <li key={pet.id} className=''>
+              <button className='flex gap-6 place-items-center p-8'>
+                <Image
+                  className='w-[65px] h-[65px] rounded-full bg-gray-400 p-4'
+                  src={pet.imageUrl}
+                  width={45}
+                  height={45}
+                  alt='image'
+                />
+                <h2 className='text-lg font-bold'>{pet.name}</h2>
+              </button>
+            </li>
+          ))}
+      
     </ul>
   );
-};
+}
---
+++ app/lib/types.ts
@@ -0,0 +1,8 @@
+export type Pet = {
+    id: string;
+    name: string;
+    imageUrl: string;
+    notes: string;
+    ownerName: string;
+    age: number;
+}
